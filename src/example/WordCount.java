da8e446b304d7a104ad9a11997596833f5cb5667
ltaoming <tl3109@nyu.edu>
2025-02-26 19:39:07 -0800

    update wordcount

--- src/example/WordCount.java
+++ src/example/WordCount.java
@@ -30,7 +30,7 @@ public class WordCount {
         private IntWritable result = new IntWritable();
 
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-           0;
+            int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
             }
