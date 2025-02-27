7cc226bb2b8b30a59ee2dacc8e39e413e37899b6
ltaoming <tl3109@nyu.edu>
2025-02-26 18:34:12 -0800

    add screenshots

--- src/example/WordCount.java
+++ src/example/WordCount.java
@@ -15,6 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package example;
+
 import java.io.IOException;
 import java.util.StringTokenizer;
 
